e67ecaaa496e73305781967b340a4fe10a173067
ArnaudovSt <telerikacademy2016@gmail.com>
2018-04-03 16:53:48 +0300

    added Remittance implementation

--- contracts/abstractions/IExchange.sol
+++ contracts/abstractions/IExchange.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
 interface IExchange {
-	function convert(bytes32 _exchangePassword, bytes32 _userPassword, uint256 _amount);
+	function convert(bytes32 _exchangePassword, bytes32 _userPassword, uint256 _amount) public;
 
 	function withdrawTokens(uint256 _amount) public;
 
--- contracts/abstractions/IRemittance.sol
+++ contracts/abstractions/IRemittance.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.18;
 
 interface IRemittance {
-	function addRemittanceRequest(bytes32 _key) public payable;
+	function addRemittanceRequest(bytes32 key) public payable;
 
-	function withdrawRemittanceRequest(bytes32 _key) public;
+	function withdrawRemittanceRequest(bytes32 _exchangePasswordHash, bytes32 _receiverPasswordHash, address _receiverAddress) public;
 
-	function withdrawEther(bytes32 _key, uint256 _amount) public;
+	function withdrawEther(bytes32 _exchangePasswordHash, bytes32 _receiverPasswordHash, address _senderAddress, uint256 _amount) external;
 
 	function setValidityDuration(uint256 _newDuration) public;
 
-	function addTrustedExchange(address _trustedExchange) public;
+	function setTrustedExchange(address _exchange, bool _isTrusted) public;
 }
