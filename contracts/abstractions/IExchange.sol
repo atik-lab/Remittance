8d04e33cb470b9d52830675d550ac0416f2e50db
ArnaudovSt <telerikacademy2016@gmail.com>
2018-04-03 13:33:11 +0300

    Added Exchange specification

---
+++ contracts/abstractions/IExchange.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+interface IExchange {
+	function convert(bytes32 _exchangePassword, bytes32 _userPassword, uint256 _amount);
+
+	function withdrawTokens(uint256 _amount) public;
+
+	function setExchangeRate(uint256 _newExchangeRate) public;
+}
--- contracts/abstractions/IRemittance.sol
+++ contracts/abstractions/IRemittance.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.4.18;
 interface IRemittance {
 	function addRemittanceRequest(bytes32 _key) public payable;
 
-	function withdraw(bytes32 _key, uint256 _amount) public;
+	function withdrawRemittanceRequest(bytes32 _key) public;
+
+	function withdrawEther(bytes32 _key, uint256 _amount) public;
 
 	function setValidityDuration(uint256 _newDuration) public;
+
+	function addTrustedExchange(address _trustedExchange) public;
 }
